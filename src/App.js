60b7e5eaf3adc022b709da9199431fdd65aadf80
Sebastian Reyes <a01663071@tec.mx>
2023-11-25 11:48:39 -0600

    Cambio de estilo de header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import CarritoList from "./components/CarritoList";
 import CarritoForm from "./components/CarritoForms";
 
 function App() {
+  const headerStyle = {
+    color: '#4A90E2',
+    textAlign: 'center',
+    fontFamily: 'Arial, sans-serif',
+    fontSize: '2.5em',
+    marginTop: '20px',
+    textShadow: '2px 2px 4px #aaa',
+    letterSpacing: '1.5px',
+  };
+
   return (
     <div>
-      <h1>Aplicación del carrito en cloud, equipo Kawasaki</h1>
+      <h1 style={headerStyle}>Aplicación del carrito en cloud, equipo Kawasaki</h1>      
       <CarritoList/>
       <CarritoForm/>
     </div>
